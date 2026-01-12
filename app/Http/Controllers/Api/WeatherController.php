a76c5fc137a402eb1b3e772ac8b99e4ca9dd4db2
hajdu.jozsef <hjozsef24@gmail.com>
2026-01-12 12:59:00 +0100

    Add check if city exists or not when searching for it

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\City;
 use App\Models\WeatherRecord;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -14,11 +15,21 @@ class WeatherController extends Controller
         $query = WeatherRecord::query();
 
         if ($request->has('city')) {
-            $query->where('name', $request->query('city'))
+            $cityName = $request->query('city');
+
+            $city = City::where('name', $cityName)->first();
+
+            if (!$city) {
+                return response()->json([
+                    'message' => 'City not found.'
+                ], 404);
+            }
+
+            $query->where('name', $cityName)
                 ->where('created_at', '>=', Carbon::now()->subDay());
         }
 
-        $records = $query->orderBy('fetched_at', 'desc')->get();
+        $records = $query->orderBy('created_at', 'desc')->get();
 
         return response()->json($records);
     }
